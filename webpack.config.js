b3dce799c804cf731742c2df7fd5c3027e499c59
Zayyan <zayyanf@gmail.com>
2022-06-07 11:13:28 -0500

    init webpack config

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  mode:'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.svg$/,
+        loader: 'svg-inline-loader'
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+        fallback: {
+            buffer: require.resolve('buffer'),
+        },
+        symlinks: false
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    library:{
+        name: "@usedispatch/forum",
+        type:'umd',
+    }
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+        Buffer: ['buffer', 'Buffer'],
+    }),
+  ],
+}

88f70ced859af4847e6c4f188196a3cc88d46fb6
Andrew Lee <andrew@usedispatch.xyz>
2022-07-13 13:25:12 -0700

    fix: proper camelCase properties in TSX file

--- src/assets/Error.tsx
+++ src/assets/Error.tsx
@@ -9,23 +9,23 @@ export function Error(props: any) {
       <path
         d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
         stroke="#E86143"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M15 9L9 15"
         stroke="#E86143"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M9 9L15 15"
         stroke="#E86143"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );

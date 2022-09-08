46f5069e2ec4bc3cd03d3241a4786668054b8157
Andrew <andrew@usedispatch.xyz>
2022-09-08 18:20:09 -0300

    feat: moderator tag for topic

--- src/components/forums/topic/TopicContent.tsx
+++ src/components/forums/topic/TopicContent.tsx
@@ -327,6 +327,7 @@ export function TopicContent(props: TopicContentProps) {
           <TopicHeader
             topic={topic}
             forum={forum}
+            participatingModerators={participatingModerators}
             isGated={currentForumAccessToken.length > 0}
           />
           <div className="moderatorToolsContainer">
@@ -437,11 +438,12 @@ export function TopicContent(props: TopicContentProps) {
 interface TopicHeaderProps {
   topic: ForumPost;
   forum: DispatchForum;
+  participatingModerators: PublicKey[] | null;
   isGated: boolean;
 }
 
 function TopicHeader(props: TopicHeaderProps) {
-  const { isGated, topic, forum } = props;
+  const { isGated, topic, forum, participatingModerators } = props;
 
   const postedAt = topic
     ? `${topic.data.ts.toLocaleDateString(undefined, {
@@ -464,6 +466,21 @@ function TopicHeader(props: TopicHeaderProps) {
               <Jdenticon value={topic.poster.toBase58()} alt="posterID" />
             </div>
             <div className="posterId">{topic.poster.toBase58()}</div>
+            &nbsp;
+            {(() => {
+              if (
+                participatingModerators &&
+                participatingModerators.some(
+                  key => key.equals(topic.poster)
+                )
+              ) {
+                return <div className={
+                  `roleLabel mod`
+                }>mod</div>
+              } else {
+                return null;
+              }
+            })()}
           </div>
           <div className="postedAt">
             Posted at: {postedAt}

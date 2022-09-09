aac99eaf8f7b2dd356290cc453e45e93ea56d72b
Andrew <andrew@usedispatch.xyz>
2022-09-09 10:27:02 -0300

    feat: useForumIdentity hook

--- src/utils/hooks.tsx
+++ src/utils/hooks.tsx
@@ -586,3 +586,32 @@ export function useUserIsMod(
 
   return userIsMod;
 }
+
+/**
+ * If a forum was created for a particular client, we can keep
+ * track of it here
+ */
+export enum ForumIdentity {
+  DegenerateApeAcademy
+}
+
+/**
+ * Return the identity of a particular forum, or `null` if it
+ * doesn't have one
+ */
+export function useForumIdentity(
+  forumId: PublicKey
+): ForumIdentity | null {
+  return useMemo(() => {
+    if (forumId.equals(
+      // TODO(andrew) put this in a constant somewhere? it is
+      // nice to have it literally specified at the point of use
+      // to avoid confusion
+      new PublicKey('DSwfRF1jhhu6HpSuzaig1G19kzP73PfLZBPLofkw6fLD')
+    )) {
+      return ForumIdentity.DegenerateApeAcademy;
+    } else {
+      return null;
+    }
+  }, [forumId]);
+}

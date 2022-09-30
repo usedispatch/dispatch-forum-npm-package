e72ee3ef4a0a2d390b3fd25c9e98b52067eee958
Zayyan Faizal <zayyan@usedispatch.xyz>
2022-09-30 08:21:17 -0500

    fix post restriction null check

--- src/utils/restrictionListHelper.tsx
+++ src/utils/restrictionListHelper.tsx
@@ -1,17 +1,39 @@
-import { PublicKey, TokenAmount } from "@solana/web3.js";
-import { PostRestriction } from "@usedispatch/client";
-import { newPublicKey } from "../utils/postbox/validateNewPublicKey";
+import { PublicKey } from '@solana/web3.js';
+import { PostRestriction } from '@usedispatch/client';
+import { newPublicKey } from '../utils/postbox/validateNewPublicKey';
 import { Result, DispatchError } from '../types/error';
 import { isError, badInputError, errorSummary } from '../utils/error';
+import isNil from 'lodash/isNil';
+
+export function restrictionListToPubkey(
+  restriction: PostRestriction,
+): PublicKey[] {
+  if (isNil(restriction)) {
+    return [];
+  }
+  return !isNil(restriction.nftOwnership)
+    ? [restriction.nftOwnership.collectionId]
+    : !isNil(restriction.nftListAnyOwnership)
+      ? restriction.nftListAnyOwnership.collectionIds
+      : !isNil(restriction.tokenOwnership)
+        ? [restriction.tokenOwnership.mint]
+        : [];
+}
+
+export function restrictionListToString(
+  restriction: PostRestriction,
+): string[] {
+  return restrictionListToPubkey(restriction).map((id) => id.toBase58());
+}
 
 // function cleans csv string, parses out into pubkeys,
 // and returns a PostRestriction object
 export function pubkeysToRestriction(
   pubkeyList: string,
-  existingRestriction?: PostRestriction
+  existingRestriction?: PostRestriction,
 ): Result<PostRestriction> {
-  const tokenCSV = pubkeyList.replace(/\s+/g, "");
-  const csvList = tokenCSV.split(",");
+  const tokenCSV = pubkeyList.replace(/\s+/g, '');
+  const csvList = tokenCSV.split(',');
 
   const newIdResults = csvList.map((token) => {
     return newPublicKey(token);
@@ -19,21 +41,21 @@ export function pubkeysToRestriction(
 
   // Collect all invalid pubkeys
   const pubkeyErrors = newIdResults.filter(
-    pkey => isError(pkey)
+    pkey => isError(pkey),
   ) as DispatchError[];
-  
+
   // If any pubkey is invalid...
   if (pubkeyErrors.length > 0) {
     return badInputError(
       pubkeyErrors
         .map(err => errorSummary(err))
-        .join(', ')
+        .join(', '),
     );
   }
 
   let newIds = newIdResults as PublicKey[];
 
-  if (existingRestriction) {
+  if (existingRestriction != null) {
     const existingIds = restrictionListToPubkey(existingRestriction);
     newIds = newIds.concat(existingIds);
   }
@@ -41,17 +63,17 @@ export function pubkeysToRestriction(
   if (newIds.length === 0) {
     return {
       null: {},
-    } as PostRestriction;
+    };
   } else if (newIds.length === 1) {
     return {
       nftOwnership: { collectionId: newIds[0] },
-    } as PostRestriction;
+    };
   } else {
     return {
       nftListAnyOwnership: {
         collectionIds: newIds,
       },
-    } as PostRestriction;
+    };
   }
 }
 
@@ -61,8 +83,8 @@ export function pubkeysToSPLRestriction(
   amount: number,
   decimals: number,
 ): Result<PostRestriction> {
-  const tokenCSV = pubkey.replace(/\s+/g, "");
-  const csvList = tokenCSV.split(",");
+  const tokenCSV = pubkey.replace(/\s+/g, '');
+  const csvList = tokenCSV.split(',');
 
   const newIdResults = csvList.map((token) => {
     return newPublicKey(token);
@@ -70,69 +92,51 @@ export function pubkeysToSPLRestriction(
 
   // Collect all invalid pubkeys
   const pubkeyErrors = newIdResults.filter(
-    pkey => isError(pkey)
+    pkey => isError(pkey),
   ) as DispatchError[];
-  
+
   // If any pubkey is invalid...
   if (pubkeyErrors.length > 0) {
     return badInputError(
       pubkeyErrors
         .map(err => errorSummary(err))
-        .join(', ')
+        .join(', '),
     );
   }
 
-  let newIds = newIdResults as PublicKey[];
+  const newIds = newIdResults as PublicKey[];
 
   const tokenAmount = amount * 10 ** decimals;
   if (newIds.length === 0) {
     return {
       null: {},
-    } as PostRestriction;
+    };
   } else if (newIds.length === 1) {
     return {
       tokenOwnership: { mint: newIds[0], amount: tokenAmount },
-    } as PostRestriction;
+    };
   } else {
     return badInputError(
       'Only one SPL token can be specified',
-      'Please remove all but one SPL token from the list'
+      'Please remove all but one SPL token from the list',
     );
   }
 }
 
 export function removePubkeyFromRestriction(
   pubkey: string,
-  restriction: PostRestriction
+  restriction: PostRestriction,
 ): PostRestriction {
   const exitingId = newPublicKey(pubkey);
   const newIds = restrictionListToPubkey(restriction).filter(
-    (id) => id !== exitingId
+    (id) => id !== exitingId,
   );
 
   const newRestriction = {
     nftListAnyOwnership: {
       collectionIds: newIds,
     },
-  } as PostRestriction;
+  };
 
   return newRestriction;
-}
-
-export function restrictionListToString(
-  restriction: PostRestriction
-): string[] {
-  return restrictionListToPubkey(restriction).map((id) => id.toBase58());
-}
-
-export function restrictionListToPubkey(
-  restriction: PostRestriction
-): PublicKey[] {
-  return restriction.nftOwnership
-    ? [restriction.nftOwnership.collectionId]
-    : restriction.nftListAnyOwnership
-    ? restriction.nftListAnyOwnership.collectionIds
-    : restriction.tokenOwnership
-    ? [restriction.tokenOwnership.mint]
-    : [];
 }

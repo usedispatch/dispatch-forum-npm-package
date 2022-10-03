e392f9790bda801c2e5dba3586f49518c0c174df
ana2201 <ana.rodriguez@xmartlabs.com>
2022-10-03 14:06:14 -0300

    [custom css] metaplex colors

--- src/utils/getCustomStyles.ts
+++ src/utils/getCustomStyles.ts
@@ -1,9 +1,12 @@
-export function getCustomStyles(collectionID: string) {
-  let result = "";
+export function getCustomStyles(collectionID: string): string {
+  let result = '';
 
   switch (collectionID) {
-    case "DSwfRF1jhhu6HpSuzaig1G19kzP73PfLZBPLofkw6fLD": 
-      result = "degenApeAcademy";
+    case 'DSwfRF1jhhu6HpSuzaig1G19kzP73PfLZBPLofkw6fLD':
+      result = 'degenApeAcademy';
+      break;
+    case 'B2qpsPkDVwS4ecDNNJ3jyzaLkXsPtpJcUnHdo9uBkrHy':
+      result = 'metaplex';
       break;
     default:
       break;
